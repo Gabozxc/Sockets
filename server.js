e49afc15dea46ed884fb2da139d05b35b67de117
Gabriel Dos Santos <gabriel.dsantos@sitiouno.com>
2022-07-04 16:51:56 -0400

    prueba2

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const listen = (port) => {
 
         socket.on("data", message => {
               if(!connections.has(socket)){
-                  console.log(`New user start in the chat, say hellow`)
+                  console.log(`New user start in the chat, say hellow in prueba sadasdasd`)
                   connections.set(socket, message)
               }
               else if(message == END){
